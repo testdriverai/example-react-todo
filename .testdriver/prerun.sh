bd050284091afbbab4b2b6e89ddc505949bbd661
Ian Jennings <ian@meetjennings.com>
2024-07-30 17:18:41 -0500

    fix prerun

--- .testdriver/prerun.sh
+++ .testdriver/prerun.sh
@@ -1,6 +1,6 @@
 npm install
 npm start
 npm install dashcam-chrome --save
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "${{inputs.url}}" --start-maximized --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --start-maximized --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
 sleep 20
 exit 
