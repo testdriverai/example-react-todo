b07b358dac1d2c3b572ad0f85ee39f93950f57c4
Ian Jennings <ian@meetjennings.com>
2024-09-12 14:49:12 -0500

    update for v4

--- .testdriver/prerun.sh
+++
@@ -1,7 +0,0 @@
-cd /Users/ec2-user/actions-runner/_work/testdriver/testdriver/.testdriver/
-npm install
-npm start
-npm install dashcam-chrome --save
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --start-maximized --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
-sleep 20
-exit 
